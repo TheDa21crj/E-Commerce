56e06d572e180a4a29a6558e5299dd0ee5fc10c2
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-13 23:34:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 connectDB();
@@ -21,7 +21,7 @@ app.use(express.json({ extended: false }));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello World");
+  res.status(200).send("Hello World");
 });
 
 app.use("/api", require("./routes/User"));
@@ -38,5 +38,5 @@ app.use("/api/Address/", require("./routes/Address"));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
